d8c25318c28520ce665d81b76e28ccc6ba2a486c
codejser <1214045596@qq.com>
2018-07-09 15:32:16 +0800

    进行web层的设计和编码；并构建前端秒杀商品列表页和详情页

---
+++ src/main/java/com/meteor/web/SeckillController.java
@@ -0,0 +1,127 @@
+package com.meteor.web;
+
+import com.meteor.dto.Exposer;
+import com.meteor.dto.SeckillExecution;
+import com.meteor.dto.SeckillResult;
+import com.meteor.entity.Seckill;
+import com.meteor.enums.SeckillEnum;
+import com.meteor.exception.RepeatKillException;
+import com.meteor.exception.SeckillCloseException;
+import com.meteor.service.ISeckillService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @Author: meteor @Date: 2018/7/9 13:36
+ */
+@Controller
+@RequestMapping("/seckill")
+public class SeckillController {
+
+    public final Logger logger = LoggerFactory.getLogger(this.getClass());
+    @Autowired
+    private ISeckillService iSeckillService;
+
+    /**
+     * 获取秒杀商品的列表页
+     * @param model
+     * @return
+     */
+    @RequestMapping(value = "/list",method = RequestMethod.GET)
+    public String list(Model model){
+
+        List<Seckill> seckillList = iSeckillService.getSeckillList();
+        model.addAttribute("list",seckillList);
+        return "list";
+    }
+
+
+    /**
+     * 返回秒杀商品的详情信息
+     * @param seckillId
+     * @param model
+     * @return
+     */
+    @RequestMapping(value = "/{seckillId}/list",method = RequestMethod.GET)
+    public String detail(@PathVariable("seckillId") Long seckillId, Model model){
+        if(seckillId == null){
+            return "redirect:/seckill/list";
+        }
+        Seckill seckill = iSeckillService.getById(seckillId);
+        if(seckill == null){
+            return "forward:/seckill/list";
+        }
+        model.addAttribute("seckill",seckill);
+        return "detail";
+    }
+
+    /**
+     * 返回秒杀接口的地址JSON
+     * @param seckillId
+     * @return
+     */
+    @RequestMapping(value = "/{seckillId}/exposer",method = RequestMethod.POST)
+    @ResponseBody
+    public SeckillResult<Exposer> exposer(Long seckillId){
+        SeckillResult<Exposer> result;
+        try {
+            Exposer exposer = iSeckillService.exportSeckillUrl(seckillId);
+            result = new SeckillResult<Exposer>(true,exposer);
+        }catch (Exception e){
+            logger.error(e.getMessage(),e);
+            result = new SeckillResult<Exposer>(false,e.getMessage());
+        }
+        return result;
+    }
+
+    /**
+     * 执行秒杀的操作
+     * @param seckillId
+     * @param md5
+     * @param phone
+     * @return
+     */
+    @RequestMapping(value = "/{seckillId}/{md5}/execution",method = RequestMethod.POST)
+    @ResponseBody
+    public SeckillResult<SeckillExecution> execute(@PathVariable("seckillId")Long seckillId,
+                                                   @PathVariable("md5") String md5,
+                                                   @CookieValue("killphone") Long phone){
+        if(phone == null){
+            return new SeckillResult<SeckillExecution>(false,"未注册");
+        }
+        try{
+            SeckillExecution seckillExecution = iSeckillService.executeSeckill(seckillId,phone,md5);
+            return new SeckillResult<SeckillExecution>(true,seckillExecution);
+        }catch (RepeatKillException e){
+            SeckillExecution seckillExecution = new SeckillExecution(seckillId, SeckillEnum.REPEAT_KILL);
+            return new SeckillResult<SeckillExecution>(false,seckillExecution);
+        }catch (SeckillCloseException e){
+            SeckillExecution seckillExecution = new SeckillExecution(seckillId, SeckillEnum.END);
+            return new SeckillResult<SeckillExecution>(false,seckillExecution);
+        }catch (Exception e){
+            logger.error(e.getMessage(),e);
+            SeckillExecution seckillExecution = new SeckillExecution(seckillId, SeckillEnum.INNER_ERROR);
+            return new SeckillResult<SeckillExecution>(false,seckillExecution);
+        }
+
+    }
+
+    /**
+     * 获取系统当前的时间
+     * @return
+     */
+    @RequestMapping(value = "/time/now",method = RequestMethod.GET)
+    @ResponseBody
+    public SeckillResult<Long> getTime(){
+        Date date = new Date();
+        return new SeckillResult<Long>(true,date.getTime());
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/jsp/list.jsp
@@ -0,0 +1,77 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<%--
+  Created by IntelliJ IDEA.
+  User: lenovo
+  Date: 2018/7/9
+  Time: 14:49
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- 上述3个meta标签*必须*放在最前面，任何其他内容都*必须*跟随其后！ -->
+    <title>秒杀商品的列表页</title>
+    <%@include file="common/tag.jsp" %>
+    <!-- Bootstrap -->
+    <link href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim 和 Respond.js 是为了让 IE8 支持 HTML5 元素和媒体查询（media queries）功能 -->
+    <!-- 警告：通过 file:// 协议（就是直接将 html 页面拖拽到浏览器中）访问页面时 Respond.js 不起作用 -->
+    <!--[if lt IE 9]>
+    <script src="https://cdn.bootcss.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+    <div class="container">
+        <div class="panel panel-default">
+            <!-- Default panel contents -->
+            <div class="panel-heading text-center">秒杀列表</div>
+            <div class="panel-body">
+                <!-- Table -->
+                <table class="table table-hover">
+                    <thead>
+                        <tr>名称</tr>
+                        <tr>库存</tr>
+                        <tr>开始时间</tr>
+                        <tr>结束时间</tr>
+                        <tr>创建时间</tr>
+                        <tr>详情页</tr>
+                    </thead>
+                    <tbody>
+                        <c:forEach var="sk" items="${list}">
+                            <tr>
+                                <td>${sk.name}</td>
+                                <td>${sk.number}</td>
+                                <td>
+                                    <fmt:formatDate value="${sk.startTime}" pattern="yyyy:MM:dd HH:mm:ss" />
+                                </td>
+                                <td>
+                                    <fmt:formatDate value="${sk.endTime}" pattern="yyyy:MM:dd HH:mm:ss" />
+                                </td>
+                                <td>
+                                    <fmt:formatDate value="${sk.createTime}" pattern="yyyy:MM:dd HH:mm:ss" />
+                                </td>
+                                <td>
+                                    <a class="btn btn-info" href="/seckill/${sk.seckillId}/detail" target="_blank">link</a>
+                                </td>
+                            </tr>
+                        </c:forEach>
+                    </tbody>
+                </table>
+            </div>
+
+        </div>
+    </div>
+
+<!-- jQuery (Bootstrap 的所有 JavaScript 插件都依赖 jQuery，所以必须放在前边) -->
+<script src="https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js"></script>
+<!-- 加载 Bootstrap 的所有 JavaScript 插件。你也可以根据需要只加载单个插件。 -->
+<script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</body>
+</html>
